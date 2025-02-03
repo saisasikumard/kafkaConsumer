1b17a86d7748e10d272fcc21ede7d4b30e9f4487
sai <dhavileswarapusaikumar@gmail.com>
2025-02-04 00:23:50 +0530

    json object handling

--- src/main/java/com/obito/kafkaConsumer/KafkaConsumerApplication.java
+++ src/main/java/com/obito/kafkaConsumer/KafkaConsumerApplication.java
@@ -1,5 +1,9 @@
 package com.obito.kafkaConsumer;
 
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.obito.kafkaImplemetation.dto.AppleRequest1;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
@@ -15,8 +19,8 @@ public class KafkaConsumerApplication {
 		SpringApplication.run(KafkaConsumerApplication.class, args);
 	}
 	@KafkaListener(topics = "apple",groupId = "payment-consumer-group")
-	public void paymentConsumer(String messege){
-		log.info("initiated the payment:"+messege);
+	public void paymentConsumer(AppleRequest1 messege) throws JsonProcessingException {
+		log.info("initiated the payment:"+new ObjectMapper().writeValueAsString(messege));
 
 	}
 }
